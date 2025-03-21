7e15cea9725ef0418e9d4f74f50dfe0969131ccd
Nils Gämperli <n.gaemperli@gmail.com>
2025-03-21 12:19:18 +0100

    spring boot setup

---
+++ src/main/java/com/stormmind/StormmindApplication.java
@@ -0,0 +1,13 @@
+package com.stormmind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StormmindApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StormmindApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stormmind/StormmindApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stormmind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StormmindApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
