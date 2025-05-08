49cb48cf4b4fe8c6b51c6eacb9e28579827ae3c8
Nils Gämperli <n.gaemperli@gmail.com>
2025-05-08 13:07:29 +0200

    commit before branch change

--- src/main/java/com/stormmind/HelloController.java
+++ src/main/java/com/stormmind/HelloController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api")
 public class HelloController {
+
     @GetMapping("/hello")
     private String sayHello() {
         return "Hello World";
